a0054da1c7fb299aa706dbc12f20970fe90ed742
DevendraKalmodiya <devendrakalmodiya1@gmail.com>
2025-06-16 17:08:25 +0530

    Update Hero.jsx

--- Client/src/components/Hero.jsx
+++ Client/src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
                 {/* text section */}
                 <div className='space-y-7 px-4 md:px-0'>
                     <h1 className='text-4xl mt-10 md:mt-0 md:text-6xl font-extrabold text-gray-200'>Explore Our <span className='text-blue-500'>14000+</span> <br /> Online courses for all</h1>
-                    <p className='text-gray-300 text-lg'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, consectetur adipiscing elit tempor ut labore</p>
+                    <p className='text-gray-300 text-lg'>Your smart hub to create, manage, and join online courses.Simple. Fast. Effective.</p>
                     <div className='inline-flex relative'>
                         <input type="text" placeholder='Search Your Course Here...' className='bg-gray-200 w-[350px] md:w-[450px] text-gray-800 p-4 pr-40 rounded-lg rounded-r-xl placeholder:text-gray-500' />
                         <button className='px-4 py-[14px] flex gap-1 items-center bg-blue-500 font-semibold absolute right-0 text-white rounded-r-lg text-xl'>Search<Search width={20} height={20} /></button>
