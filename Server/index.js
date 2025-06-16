0dddaadde28795bbb078f0ef4e33eb1c2fd1d7b8
DevendraKalmodiya <devendrakalmodiya1@gmail.com>
2025-06-16 17:02:18 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -24,7 +24,7 @@ app.use(express.json())
 app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
 const allowedOrigins = [
-  "http://localhost:5000",
+  "http://localhost:5173",
   "http://localhost:5174"
 ];
 
