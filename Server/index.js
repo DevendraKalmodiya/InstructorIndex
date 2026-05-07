13ecfa6d7064975d30fd518a181ae52a33042b8a
Devendra Kalmodiya <Devendrakalmodiya1@gmail.com>
2026-05-08 05:24:53 +0530

    Deployment fix

--- Server/index.js
+++ Server/index.js
@@ -27,7 +27,7 @@ app.use(express.json())
 app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({
-    origin:["http://localhost:5173", "http://localhost:3000"],
+    origin:["https://instructorindex.vercel.app", "http://localhost:5173", "http://localhost:3000"],
     credentials:true
 }))
 
